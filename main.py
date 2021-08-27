145cedd3a7d592711f4f498d39cf31fb5e92d3e8
Amin <aminoffline@yahoo.com>
2021-08-28 00:17:02 +0430

    small fix

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import re
 
 def email_check(email):
     email_regex = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Z|a-z]{2,}\b')
-    if email_regex.fullmatch(email) and len(list(re.finditer(r'([^\d]+)@.',email))) != 0:
-        print("OK")
+    if email_regex.fullmatch(email) and len(list(re.finditer(r'(\D*)@.',email))) != 0:
+        print('OK')
     else:
         print('WRONG')
 
-email_check(email = input('Please Enter Your Email: '))
+email_check(email = input())
